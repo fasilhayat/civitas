95237944e0b9bbff158818d2cd416472deb02406
Fasil Malik Hayat <fasil.hayat@gmail.com>
2025-04-16 15:28:53 +0200

    Looking beautiful

--- Civitas.Api/Core/Entities/AccessRole.cs
+++ Civitas.Api/Core/Entities/AccessRole.cs
@@ -1,9 +1,12 @@
 ﻿namespace Civitas.Api.Core.Entities;
 
+/// <summary>
+/// 
+/// </summary>
 public class AccessRole
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public string? ThirdPartyId { get; set; }
 
     // Optional: If you want to include a reference back to AccessLevelControl
