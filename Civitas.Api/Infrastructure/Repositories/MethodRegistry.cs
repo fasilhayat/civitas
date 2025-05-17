abe6eb7f47dbafa16bf521f924bd8ab6af7c53e8
Fasil Malik Hayat <fasil.hayat@gmail.com>
2025-05-17 07:38:51 +0200

    Comments

--- Civitas.Api/Infrastructure/Repositories/MethodRegistry.cs
+++ Civitas.Api/Infrastructure/Repositories/MethodRegistry.cs
@@ -9,11 +9,22 @@ public class MethodRegistry : IMethodRegistry
 {
     private readonly Dictionary<string, Func<object?, Task<bool>>> _handlers = new();
 
+    /// <summary>
+    /// Registers a method handler with a specified key.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="handler"></param>
     public void Register(string key, Func<object?, Task<bool>> handler)
     {
         _handlers[key] = handler;
     }
 
+    /// <summary>
+    /// Gets the handler for a specified key.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
     public Func<object?, Task<bool>> GetHandler(string key)
     {
         if (!_handlers.TryGetValue(key, out var handler))
