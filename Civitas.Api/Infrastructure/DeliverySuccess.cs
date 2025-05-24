78f5e04bb3aa39a90ee2b0efc0d02ca3a2f1119c
Fasil Malik Hayat <fasil.hayat@gmail.com>
2025-05-24 08:54:40 +0200

    Comments

--- Civitas.Api/Infrastructure/DeliverySuccess.cs
+++ Civitas.Api/Infrastructure/DeliverySuccess.cs
@@ -1,7 +1,7 @@
 ﻿namespace Civitas.Api.Infrastructure;
 
 /// <summary>
-/// 
+/// This record represents a successful delivery of a message.
 /// </summary>
-/// <param name="DeliveryId"></param>
+/// <param name="DeliveryId">The unique identifier for the delivery.</param>
 public sealed record DeliverySuccess(long DeliveryId);
