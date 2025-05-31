865c7bb2e924a5bfeabcdf3c0a70cfd04dc167aa
Fasil Malik Hayat <fasil.hayat@gmail.com>
2025-05-31 07:04:27 +0200

    Added comments

--- Civitas.Api/Infrastructure/Actors/DeliveryConfirmed.cs
+++ Civitas.Api/Infrastructure/Actors/DeliveryConfirmed.cs
@@ -1,5 +1,13 @@
 ﻿namespace Civitas.Api.Infrastructure.Actors;
 
+/// <summary>
+/// This record represents a message confirming the delivery of a method call.
+/// </summary>
+/// <param name="DeliveryId"></param>
 public record DeliveryConfirmed(long DeliveryId);
 
+/// <summary>
+/// This record represents a message to check if a delivery has been received.
+/// </summary>
+/// <param name="DeliveryId"></param>
 public record ReceiveDeliveryCheck(long DeliveryId);
