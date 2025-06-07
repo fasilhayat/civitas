bf6b0ad76cf479aff4b9f97ef5d22d3d65900a5d
Fasil Malik Hayat <fasil.hayat@gmail.com>
2025-06-07 07:47:27 +0200

    Indent fixes

--- Civitas.Api/wwwroot/swagger-ui/custom.js
+++ Civitas.Api/wwwroot/swagger-ui/custom.js
@@ -3,14 +3,14 @@
         setTimeout(function () {
             var logo = document.getElementsByClassName('link')[0];
             if (logo) {
-                logo.href = "https://www.forca.dk";
+                logo.href = "https://www.nykredit.dk";
                 logo.target = "_blank";
 
                 var svgLogo = logo.children[0];
                 if (svgLogo && svgLogo.tagName === "svg") {
                     // Replace SVG with an IMG element
                     var imgLogo = document.createElement("img");
-                    imgLogo.alt = "FORCA Swagger";
+                    imgLogo.alt = "Nykredit Swagger";
                     imgLogo.src = "/swagger-ui/logo-nykredit.png";
                     imgLogo.style.height = "30px";
                     logo.replaceChild(imgLogo, svgLogo);
